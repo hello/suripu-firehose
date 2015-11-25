9accb9d8e6fc09e0cc92ddfd3b00010b4584d407
jakepic1 <jake@sayhello.com>
2015-11-25 13:54:24 -0600

    Add factory

---
+++ src/main/java/com/hello/suripu/firehose/sense/SenseDataRecordProcessorFactory.java
@@ -0,0 +1,29 @@
+package com.hello.suripu.firehose.sense;
+
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessor;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorFactory;
+import com.hello.suripu.core.db.DeviceDataIngestDAO;
+import com.hello.suripu.core.db.MergedUserInfoDynamoDB;
+
+/**
+ * Created by jakepiccolo on 11/25/15.
+ */
+public class SenseDataRecordProcessorFactory implements IRecordProcessorFactory {
+
+    private final DeviceDataIngestDAO deviceDataDAO;
+    private final MergedUserInfoDynamoDB mergedInfoDynamoDB;
+    private final Integer maxRecords;
+
+    public SenseDataRecordProcessorFactory(final MergedUserInfoDynamoDB mergedUserInfoDynamoDB,
+                                           final DeviceDataIngestDAO deviceDataDAO,
+                                           final Integer maxRecords) {
+        this.deviceDataDAO = deviceDataDAO;
+        this.mergedInfoDynamoDB = mergedUserInfoDynamoDB;
+        this.maxRecords = maxRecords;
+    }
+
+    @Override
+    public IRecordProcessor createProcessor() {
+        return new SenseDataRecordProcessor(mergedInfoDynamoDB, deviceDataDAO , maxRecords);
+    }
+}
