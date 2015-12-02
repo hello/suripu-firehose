da6b3e4db6ddec97ff1b9464c3620d7a01c14052
jakepic1 <jake@sayhello.com>
2015-12-02 14:33:49 -0800

    Switch back to current branch after releasing

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+CURR_BRANCH=`git rev-parse --abbrev-ref HEAD`
 git checkout master
 git pull
 git checkout PRODUCTION
 git merge master --no-edit
 git push origin PRODUCTION
+git checkout $CURR_BRANCH
